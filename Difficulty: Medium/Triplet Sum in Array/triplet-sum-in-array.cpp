4e2fe62caf4c72e7be975477f5e9946984e2e747
Madhavan <154647865+Madhavan0210@users.noreply.github.com>
2024-07-09 02:28:55 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Triplet Sum in Array/triplet-sum-in-array.cpp
@@ -0,0 +1,58 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+
+    // Should return true if there exists a triplet in the
+    // array arr[] which sums to x. Otherwise false
+    bool find3Numbers(int nums[], int n, int x) {
+        // Your Code Herevector<vector<int>>ans;
+        vector<vector<int>>ans;
+        sort(nums,nums+n);
+        for(int i=0;i<n;i++)
+        {
+            if(i>0 && nums[i]==nums[i-1]) continue;
+            int j=i+1;
+            int k=n-1;
+            while(j<k)
+            {
+                int sum=nums[i]+nums[j]+nums[k];
+                if(sum<x) j++;
+                else if(sum>x) k--;
+                else
+                {
+                    vector<int>temp={nums[i],nums[j],nums[k]};
+                    ans.push_back(temp);
+                    j++;
+                    k--;
+                    while(j<k && nums[j]==nums[j-1]) j++;
+                    while(j<k && nums[k]==nums[k+1]) k--;
+                }
+            }
+        }
+        if(ans.size()!=0) return 1;
+        else return 0;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int T;
+    cin >> T;
+    while (T--) {
+        int n, X;
+        cin >> n >> X;
+        int i, A[n];
+        for (i = 0; i < n; i++)
+            cin >> A[i];
+        Solution ob;
+        cout << ob.find3Numbers(A, n, X) << endl;
+    }
+}
+
+// } Driver Code Ends
