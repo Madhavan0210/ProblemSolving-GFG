1886275700d754133cd2541b8fc5f376747c7a01
Madhavan <154647865+Madhavan0210@users.noreply.github.com>
2024-08-06 19:43:02 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Find the element that appears once/find-the-element-that-appears-once.cpp
@@ -0,0 +1,40 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+class Solution {
+  public:
+    int search(int n, int A[]) {
+        // code
+          int xorr = 0;
+        for(int i=0;i<n;i++)
+       {
+         xorr=xorr^A[i];
+       }
+      return xorr; 
+    }
+};
+
+//{ Driver Code Starts.
+
+// Driver program
+int main() {
+    int t, len;
+    cin >> t;
+    while (t--) {
+        cin >> len;
+        int arr[len];
+        for (int i = 0; i < len; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        cout << ob.search(len, arr) << '\n';
+    }
+    return 0;
+}
+
+// } Driver Code Ends
