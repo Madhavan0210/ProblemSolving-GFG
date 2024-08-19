10626ca8d3c2fc6742fb7ab394b3e1e2d793f12a
Madhavan <154647865+Madhavan0210@users.noreply.github.com>
2024-08-19 19:20:34 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Sum Of Digits/sum-of-digits.cpp
@@ -0,0 +1,35 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+public:
+    int sumOfDigits(int N){
+        //code here
+            int cnt=0;
+     while(N>0)
+     {
+      int temp=N%10;
+      cnt+=temp;
+      N=N/10;
+     }
+     return cnt;
+    }
+};
+
+//{ Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int N;
+        cin>>N;
+        Solution ob;
+        cout << ob.sumOfDigits(N) << endl;
+    }
+    return 0; 
+}
+// } Driver Code Ends
