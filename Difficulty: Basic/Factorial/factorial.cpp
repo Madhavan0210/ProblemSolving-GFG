5fbcdb81f71d0f899421b9513a2a5b9d6d54728d
Madhavan <154647865+Madhavan0210@users.noreply.github.com>
2024-08-21 18:14:16 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Factorial/factorial.cpp
@@ -0,0 +1,34 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+public:
+    long long int factorial(int N){
+        //code here
+        long long tot=1;
+        while(N>0)
+        {
+            tot*=N;
+            N--;
+        }
+        return tot;
+    }
+};
+
+//{ Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int N;
+        cin>>N;
+        Solution ob;
+        cout << ob.factorial(N) << endl;
+    }
+    return 0; 
+}
+// } Driver Code Ends
