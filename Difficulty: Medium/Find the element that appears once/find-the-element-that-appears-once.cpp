3ab20730432f9965e42184609c2e5454fe94fa94
Madhavan <154647865+Madhavan0210@users.noreply.github.com>
2024-09-10 10:05:31 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Find the element that appears once/find-the-element-that-appears-once.cpp
+++ Difficulty: Medium/Find the element that appears once/find-the-element-that-appears-once.cpp
@@ -8,14 +8,31 @@ using namespace std;
 // User function template for C++
 class Solution {
   public:
-    int search(int n, int A[]) {
+    int search(int n, int nums[]) {
         // code
-          int xorr = 0;
-        for(int i=0;i<n;i++)
-       {
-         xorr=xorr^A[i];
-       }
-      return xorr; 
+        if(n==1) return nums[0];
+        if(nums[0]!=nums[1]) return nums[0];
+        if(nums[n-1]!=nums[n-2]) return nums[n-1];
+        int low=1,high=n-2;
+        while(low<=high)
+        {
+         int mid=(low+high)/2;
+         if(nums[mid]!=nums[mid+1] && nums[mid]!=nums[mid-1]) 
+         {
+            return nums[mid];
+         }
+         //left
+         if((mid%2==1 && nums[mid]==nums[mid-1])||(mid%2==0 && nums[mid]==nums[mid+1]))   
+         {
+            low=mid+1;
+         } 
+         //right
+         else
+         {
+            high=mid-1;
+         }
+        }
+        return -1;
     }
 };
 
