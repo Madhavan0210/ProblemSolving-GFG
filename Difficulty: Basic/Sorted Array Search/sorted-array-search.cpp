7ce059900ef891753f93c4d17a135c6e9434f6ac
Madhavan <154647865+Madhavan0210@users.noreply.github.com>
2024-10-13 18:37:29 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Sorted Array Search/sorted-array-search.cpp
+++ Difficulty: Basic/Sorted Array Search/sorted-array-search.cpp
@@ -13,7 +13,7 @@ class Solution {
     int searchInSorted(int arr[], int N, int K) {
 
         // Your code here
-         for(int i=0;i<N;i++)
+          for(int i=0;i<N;i++)
        {
          if(arr[i]==K) return 1;  
        }
