aa848744a04e5d61b02d7a9c49e5c203439c6cd9
Madhavan <154647865+Madhavan0210@users.noreply.github.com>
2024-10-21 23:04:00 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Array Duplicates/array-duplicates.cpp
@@ -0,0 +1,63 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    vector<int> findDuplicates(vector<int>& arr) {
+        // code here
+        vector<int>res;
+int n=arr.size();
+int freq[n]={0};
+for(int i=0;i<n;i++){
+    freq[arr[i]]++;
+}
+for(int i=0;i<n;i++){
+    if(freq[i]>1){
+        res.push_back(i);
+    }
+}
+if(res.empty())
+return res;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t; // Number of test cases
+    cin >> t;
+    cin.ignore(); // Ignore the newline after the integer input
+    while (t--) {
+        vector<int> a;
+        string input;
+
+        // Input format: first number n followed by the array elements
+        getline(cin, input);
+        stringstream ss(input);
+        int num;
+        while (ss >> num)
+            a.push_back(num); // Read the array elements from input string
+
+        Solution obj;
+        vector<int> result = obj.findDuplicates(a);
+
+        // Print the result in the required format
+        if (result.empty()) {
+            cout << "[]"; // Print empty brackets if no duplicates are found
+        } else {
+            for (int i = 0; i < result.size(); i++) {
+                if (i != 0)
+                    cout << " ";
+                cout << result[i];
+            }
+        }
+        cout << endl; // Ensure new line after each test case output
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends
