55e0cd34649e8a163f0b2db2dd0d3396948fc6c4
Madhavan <154647865+Madhavan0210@users.noreply.github.com>
2024-12-25 20:10:46 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Merge K sorted linked lists/merge-k-sorted-linked-lists.cpp
@@ -0,0 +1,116 @@
+//{ Driver Code Starts
+// C++ program to merge k sorted arrays of size n each
+#include <bits/stdc++.h>
+using namespace std;
+
+// A Linked List node
+struct Node {
+    int data;
+    Node* next;
+
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+/* Function to print nodes in a given linked list */
+void printList(Node* node) {
+    while (node != NULL) {
+        printf("%d ", node->data);
+        node = node->next;
+    }
+    cout << endl;
+}
+
+
+// } Driver Code Ends
+/*Linked list Node structure
+
+struct Node
+{
+    int data;
+    Node* next;
+
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+
+};
+*/
+
+class Solution {
+  public:
+    // Function to merge K sorted linked list.
+    Node* mergeKLists(vector<Node*>& lists) {
+        // Your code here
+         priority_queue<pair<int,Node*>,vector<pair<int,Node*>>,greater<pair<int,Node*>>>pq;
+        for(int i=0;i<lists.size();i++)
+        {
+            if(lists[i])
+               pq.push({lists[i]->data,lists[i]});
+        }
+        Node* dummynode=new Node(-1);
+        Node* temp=dummynode;
+        while(!pq.empty())
+        {
+          auto it=pq.top();
+          pq.pop();
+          if(it.second->next)
+          {
+            pq.push({it.second->next->data,it.second->next});
+          }
+          temp->next=it.second;
+          temp=temp->next;
+        }
+        return dummynode->next;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+// Driver program to test the above functions
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<Node*> arr;
+        vector<int> nums;
+        string input;
+
+        getline(cin, input); // Read the entire line for the array elements
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            nums.push_back(number);
+        }
+        int ind = 0;
+        int N = nums.size();
+
+        while (ind < N) {
+            int n = nums[ind++];
+            int x = nums[ind++];
+            Node* head = new Node(x);
+            Node* curr = head;
+            n--;
+
+            for (int i = 0; i < n; i++) {
+                x = nums[ind++];
+                Node* temp = new Node(x);
+                curr->next = temp;
+                curr = temp;
+            }
+            arr.push_back(head);
+        }
+
+        Solution obj;
+        Node* res = obj.mergeKLists(arr);
+        printList(res);
+    }
+    return 0;
+}
+
+// } Driver Code Ends
