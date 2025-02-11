16978bc9cb8d7d32ae144afebfc1e92c38268ec2
Madhavan <154647865+Madhavan0210@users.noreply.github.com>
2025-02-11 08:55:06 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Check for Binary String/check-for-binary-string.cpp
+++ Difficulty: Basic/Check for Binary String/check-for-binary-string.cpp
@@ -10,10 +10,9 @@ class Solution {
   public:
     bool isBinary(string& s) {
         // Your code here
-        for(int i=0;i<s.length();i++){
-           if(s[i] != '0' && s[i]!= '1')
-           return false;
-          
+        for(int i=0;s[i]!='\0';i++)
+        {
+            if(s[i]!='0' && s[i]!='1') return false;
         }
         return true;
     }
