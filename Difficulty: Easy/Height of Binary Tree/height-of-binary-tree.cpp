0b6e034e24095f3867f44f0b7fa9e478838aca5e
Madhavan <154647865+Madhavan0210@users.noreply.github.com>
2025-06-27 11:41:44 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Height of Binary Tree/height-of-binary-tree.cpp
@@ -0,0 +1,27 @@
+/*
+class Node {
+public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+*/
+class Solution {
+  public:
+    int helper(Node* root)
+    {
+         if(root==NULL) return 0;
+        int l=helper(root->left);
+        int r=helper(root->right);
+        return 1+max(l,r);
+    }
+    int height(Node* root) {
+        // code here
+         return helper(root)-1;
+    }
+};
