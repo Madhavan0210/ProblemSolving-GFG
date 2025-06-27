0888a5bf0f178885aa06fc7fb10158f1ccd0c892
Madhavan <154647865+Madhavan0210@users.noreply.github.com>
2025-06-27 08:38:17 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/C++ STL | Set 3 (map)/c-stl-set-3-map.cpp
+++ Difficulty: Basic/C++ STL | Set 3 (map)/c-stl-set-3-map.cpp
@@ -1,7 +1,7 @@
 /* Adds a value with key x and value y to the map*/
 void add_value(map<int, int> &m, int x, int y) {
     // Your code here
-     m[x]=y;
+    m[x]=y;
 }
 
 /* Returns the value of the key
@@ -9,20 +9,13 @@ void add_value(map<int, int> &m, int x, int y) {
 int find_value(map<int, int> &m, int x) {
     
     // Your code here
-     auto it=m.find(x);
-  if(it!=m.end()){
-      return it->second;
-  }
-  else{
-      return -1;
-  }
+    if(m.find(x)!=m.end()) return m[x];
+    return -1;
 }
 
 /* Prints contents of the map ie keys and values*/
     
 void print_contents(map<int, int> &m) {
     // Your code here
-      for(auto i:m){
-       cout<<i.first<<" "<<i.second<<" ";
-   }
+    for(auto it : m) cout<<it.first<<" "<<it.second<<" ";
 }
