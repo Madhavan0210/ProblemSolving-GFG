ab609bd162740e1bad9e94f69be19be232a24f95
Madhavan <154647865+Madhavan0210@users.noreply.github.com>
2025-09-15 07:02:00 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Power of Four/power-of-four.cpp
@@ -0,0 +1,9 @@
+class Solution {
+  public:
+    int isPowerOfFour(unsigned int n) {
+        // Your code goes here
+        if(n==0) return 0;
+        while(n%4==0) n=n/4;
+        return n==1?1:0;
+    }
+};
