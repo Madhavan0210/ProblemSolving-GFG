3d2b7431a87c8fa46515e72ca659e664d3ea84f9
Madhavan <154647865+Madhavan0210@users.noreply.github.com>
2025-10-18 08:19:53 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/At Least K Occurrences/at-least-k-occurrences.cpp
@@ -0,0 +1,13 @@
+class Solution {
+  public:
+    int firstElementKTime(vector<int>& arr, int k) {
+        // code here
+        map<int,int>mpp;
+        for(int i=0;i<arr.size();i++)
+        {
+            mpp[arr[i]]++;
+            if(mpp[arr[i]]==k) return arr[i];
+        }
+        return -1;
+    }
+};
