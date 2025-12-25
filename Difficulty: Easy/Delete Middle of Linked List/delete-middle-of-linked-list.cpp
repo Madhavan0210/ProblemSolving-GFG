0dbd772f834611b2146b108a0f3a1c105e1f11a7
Madhavan <154647865+Madhavan0210@users.noreply.github.com>
2025-12-25 23:02:05 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Delete Middle of Linked List/delete-middle-of-linked-list.cpp
+++ Difficulty: Easy/Delete Middle of Linked List/delete-middle-of-linked-list.cpp
@@ -1,19 +1,3 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-struct Node {
-    int data;
-    struct Node* next;
-
-    Node(int x) {
-        data = x;
-        next = NULL;
-    }
-};
-
-
-// } Driver Code Ends
 /* Link list Node:
 
 struct Node
@@ -34,7 +18,7 @@ class Solution {
   public:
     Node* deleteMid(Node* head) {
         // Your Code Here
-        Node* slow=head;
+         Node* slow=head;
         Node* fast=head;
         Node* prev=NULL;
         if(head==NULL || head->next==NULL) return NULL;
@@ -50,42 +34,3 @@ class Solution {
         return head;
     }
 };
-
-//{ Driver Code Starts.
-
-void printList(Node* node) {
-    while (node != NULL) {
-        cout << node->data << " ";
-        node = node->next;
-    }
-    cout << "\n";
-}
-
-int main() {
-    int t;
-    cin >> t;
-    cin.ignore();
-    while (t--) {
-        vector<int> arr;
-        string input;
-        getline(cin, input);
-        stringstream ss(input);
-        int number;
-        while (ss >> number) {
-            arr.push_back(number);
-        }
-        struct Node* head = new Node(arr[0]);
-        struct Node* tail = head;
-        for (int i = 1; i < arr.size(); ++i) {
-            tail->next = new Node(arr[i]);
-            tail = tail->next;
-        }
-        Solution obj;
-        head = obj.deleteMid(head);
-        printList(head);
-        cout << "~\n";
-    }
-    return 0;
-}
-
-// } Driver Code Ends
