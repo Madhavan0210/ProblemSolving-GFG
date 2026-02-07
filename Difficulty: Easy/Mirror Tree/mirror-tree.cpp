f6723e6d1ec83cd57e081bdb897f5978d5b3391a
Madhavan <154647865+Madhavan0210@users.noreply.github.com>
2026-02-07 22:53:43 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Mirror Tree/mirror-tree.cpp
+++ Difficulty: Easy/Mirror Tree/mirror-tree.cpp
@@ -14,10 +14,9 @@ public:
 
 class Solution {
   public:
-    // Function to convert a binary tree into its mirror tree.
     void mirror(Node* root) {
         // code here
-         if(!root) return;
+          if(!root) return;
         mirror(root->left);
         mirror(root->right);
         Node* temp=root->left;
