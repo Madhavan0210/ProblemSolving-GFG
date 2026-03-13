6a1d508c609496fe31136352e3924ace40f55d2a
Madhavan <154647865+Madhavan0210@users.noreply.github.com>
2026-03-13 21:52:54 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Meta Strings/meta-strings.cpp
+++ Difficulty: Easy/Meta Strings/meta-strings.cpp
@@ -1,7 +1,7 @@
 
 bool metaStrings(string A, string B) {
     // your code here
-      if (A.length() != B.length()) return false;
+    if (A.length() != B.length()) return false;
         if (A == B && set<char>(A.begin(), A.end()).size() < A.size()) return false;
         vector<int> dif;
         for (int i = 0; i < A.length(); ++i) if (A[i] != B[i]) dif.push_back(i);
