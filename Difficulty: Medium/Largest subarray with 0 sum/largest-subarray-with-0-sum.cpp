351f0e7a23f5d834b14fce226345cd11bb065577
Madhavan <154647865+Madhavan0210@users.noreply.github.com>
2026-03-20 22:41:22 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Largest subarray with 0 sum/largest-subarray-with-0-sum.cpp
+++ Difficulty: Medium/Largest subarray with 0 sum/largest-subarray-with-0-sum.cpp
@@ -1,18 +1,9 @@
-//{ Driver Code Starts
-// Initial Template for C++
-
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-/*You are required to complete this function*/
-
 class Solution {
   public:
-    int maxLen(vector<int>& arr, int n) {
-        // Your code here
-        unordered_map<int,int>mpp;
+    int maxLength(vector<int>& arr) {
+        // code here
+        int n=arr.size();
+          unordered_map<int,int>mpp;
         int maxi=0;
         int sum=0;
         for(int i=0;i<n;i++)
@@ -35,24 +26,3 @@ class Solution {
         return maxi;
     }
 };
-
-
-//{ Driver Code Starts.
-
-int main() {
-    int t;
-    cin >> t;
-    while (t--) {
-        int m;
-        cin >> m;
-        vector<int> array1(m);
-        for (int i = 0; i < m; ++i) {
-            cin >> array1[i];
-        }
-        Solution ob;
-        cout << ob.maxLen(array1, m) << endl;
-    }
-    return 0;
-}
-
-// } Driver Code Ends
