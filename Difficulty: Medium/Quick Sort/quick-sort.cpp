fbcf97e56008d66f2fb069ed674074b6d80b1e67
Madhavan <154647865+Madhavan0210@users.noreply.github.com>
2026-03-21 23:09:48 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Quick Sort/quick-sort.cpp
+++ Difficulty: Medium/Quick Sort/quick-sort.cpp
@@ -1,38 +1,19 @@
-//{ Driver Code Starts
-#include <stdio.h>
-#include <bits/stdc++.h>
-using namespace std;
-
-/* Function to print an array */
-void printArray(int arr[], int size)
-{
-    int i;
-    for (i=0; i < size; i++)
-        printf("%d ", arr[i]);
-    printf("\n");
-}
-
-// } Driver Code Ends
-
-class Solution
-{
-    public:
-    //Function to sort an array using quick sort algorithm.
-    void quickSort(int arr[], int low, int high)
-    {
+class Solution {
+  public:
+    void quickSort(vector<int>& arr, int low, int high) {
         // code here
-        if(low<high){
+         if(low<high){
        int partitionn=partition(arr,low,high);
        quickSort(arr,low,partitionn-1);
        quickSort(arr,partitionn+1,high); 
     }
     }
-    
-    public:
-    int partition (int arr[], int low, int high)
-    {
-       // Your code here
-       int pivot=arr[low];
+
+  public:
+    int partition(vector<int>& arr, int low, int high) {
+        
+        // code here
+        int pivot=arr[low];
    int i=low;
    int j=high;
    while(i<j)
@@ -50,22 +31,5 @@ class Solution
    swap(arr[low],arr[j]);
    return j;
     }
+    
 };
-
-
-//{ Driver Code Starts.
-int main()
-{
-    int arr[1000],n,T,i;
-    scanf("%d",&T);
-    while(T--){
-    scanf("%d",&n);
-    for(i=0;i<n;i++)
-      scanf("%d",&arr[i]);
-      Solution ob;
-    ob.quickSort(arr, 0, n-1);
-    printArray(arr, n);
-    }
-    return 0;
-}
-// } Driver Code Ends
