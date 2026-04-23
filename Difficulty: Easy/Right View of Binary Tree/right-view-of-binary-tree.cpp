f6719f545b6f6b9904a326e8c3bd4556eaced55b
Madhavan <154647865+Madhavan0210@users.noreply.github.com>
2026-04-23 23:07:46 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Right View of Binary Tree/right-view-of-binary-tree.cpp
+++ Difficulty: Easy/Right View of Binary Tree/right-view-of-binary-tree.cpp
@@ -1,22 +1,21 @@
-/* A binary tree node has data, pointer to left child
-   and a pointer to right child
-struct Node
-{
+/*
+class Node {
+  public:
     int data;
-    struct Node* left;
-    struct Node* right;
+    Node* left;
+    Node* right;
 
-    Node(int x){
-        data = x;
-        left = right = NULL;
+    Node(int val) {
+        data = val;
+        left = nullptr;
+        right = nullptr;
     }
-}; */
+};
+*/
 
-// Should return  right view of tree
 class Solution {
   public:
-    // Function to return list containing elements of right view of binary tree.
-     void helper(Node* root,vector<int>&vec,int level)
+    void helper(Node* root,vector<int>&vec,int level)
     {
      if(root==NULL) return;
      if(vec.size()==level) vec.push_back(root->data);
@@ -24,8 +23,8 @@ class Solution {
      helper(root->left,vec,level+1);
     }
     vector<int> rightView(Node *root) {
-        // Your Code here
-        vector<int>vec;
+        //  code here
+         vector<int>vec;
       helper(root,vec,0);
       return vec;
     }
